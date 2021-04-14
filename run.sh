7e38c31343722748fbb58ff7dfb4c17bd63f960e
amfchef <jakobtj@hotmail.com>
2021-04-14 11:19:55 +0200

    html works!

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 ansible-playbook -i inventory.yaml playbooks/lighttpd.yaml
-ansible-playbook -i inventory.yaml playbooks/copy_files.yaml
+#ansible-playbook -i inventory.yaml playbooks/copy_files.yaml
